52b0318cc34f135efa3f3ebb004006a897d268d1
twentiecker <twentiecker@gmail.com>
2023-02-27 21:26:42 +0700

    create blockchain and test

---
+++ blockchain/blockchain.test.js
@@ -0,0 +1,36 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe('Blockchain', () => {
+  let bc;
+  beforeEach(() => {
+    bc = new Blockchain();
+    bc2 = new Blockchain();
+  });
+
+  it('starts with genesis block', () => {
+    expect(bc.chain[0]).toEqual(Block.genesis());
+  });
+
+  it('adds a new block', () => {
+    const data = 'foo';
+    bc.addBlock(data);
+    expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+  });
+
+  it('validates a valid chain', () => {
+    bc2.addBlock('foo');
+    expect(bc.isValidChain(bc2.chain)).toBe(true);
+  });
+
+  it('invalidate a chain with a corrupt genesis block', () => {
+    bc2.chain[0].data = 'Bad Data';
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+
+  it('invalidate corrupt chain', () => {
+    bc2.addBlock('foo');
+    bc2.chain[1].data = 'Not foo';
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+});
--- dev-test.js
+++ dev-test.js
@@ -1,3 +1,3 @@
-const Block = require('./block');
+const Block = require('./blockchain/block');
 const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
 console.log(fooBlock.toString());
