349e5c2cecdeb8e5a0486f7bc59b9a143faf3c71
twentiecker <twentiecker@gmail.com>
2023-02-27 17:57:27 +0700

    add genesis class

--- block.js
+++ block.js
@@ -13,6 +13,10 @@ class Block {
     Hash : ${this.hash.substring(0, 10)}
     Data : ${this.data}`;
   }
+
+  static genesis() {
+    return new this('Genesis time', '-----', 'f1rst_h4sh', []);
+  }
 }
 
 module.exports = Block;
